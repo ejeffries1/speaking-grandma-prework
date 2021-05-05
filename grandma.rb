16faa692f3cc35eb980ce488b0ee411a82bf080c
Evan Jeffries <ejeffries14@gmail.com>
2021-05-05 04:38:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,7 @@ def speak_to_grandma(phrase)
    return "HUH?! SPEAK UP, SONNY!"
    elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  else "NO, NOT SINCE 1938!"
  end
 end
 # Whatever you say to grandma, she should respond with
