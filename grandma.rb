eb49bb3909b3dc41f606746a1b65ad06a0891756
Evan Jeffries <ejeffries14@gmail.com>
2021-05-05 04:36:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase == "Hi Nana, how are you?" || phrase == "Hi!"
-   puts "HUH?! SPEAK UP, SONNY!"
+   return "HUH?! SPEAK UP, SONNY!"
  end
 end
 # Whatever you say to grandma, she should respond with
