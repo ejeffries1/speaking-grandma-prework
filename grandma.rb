dba11b475815ca4973e6d06cdd238380d8aaf574
Evan Jeffries <ejeffries14@gmail.com>
2021-05-05 04:33:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,7 @@ def speak_to_grandma(phrase)
    puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "Hi Nana, how are you?" || phrase == "Hi!"
    puts "HUH?! SPEAK UP, SONNY!"
+ end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
